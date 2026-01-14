3c7d65523e0a04c6dafda569940e2b8d613aeb74
julius mutinda <juliusmwendwa082@gmail.com>
2026-01-14 08:58:28 +0300

    Update server files

--- server/app.py
+++ server/app.py
@@ -3,9 +3,9 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from server.models import db
 
-# create a Flask application instance 
+# create a Flask application instance
 app = Flask(__name__)
 
 # configure the database connection to the local file app.db
@@ -14,11 +14,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 # configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# bind the SQLAlchemy object to the app
+db.init_app(app)
+
 # create a Migrate object to manage schema modifications
 migrate = Migrate(app, db)
-
-# initialize the Flask application to use the database
-db.init_app(app)
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 #server/seed.py
 
-from app import app
-from models import db, Pet
+from server.app import app
+from server.models import db, Pet
+
 
 with app.app_context():
 
@@ -10,9 +11,9 @@ with app.app_context():
     pets = []
 
     # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name="Fido", species="Dog"))
+    pets.append(Pet(name="Whiskers", species="Cat"))
+    pets.append(Pet(name="Hermie", species="Hamster"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
